889a83cca681d7a0a7f6eed71d532317d254d9ed
Suryaa <suryaa.prakash0369@gmail.com>
2025-09-22 23:06:23 +0530

    new_function is new model

--- ml_model.py
+++ ml_model.py
@@ -7,4 +7,7 @@ def new_function():
     print("this is a new function added in the ml model file")  
 
 def another_function():
-    print("this is another function added in the ml model file")        
+    print("this is another function added in the ml model file")   
+
+
+print('new_function is performing the best')     
