8328f7a6e2ace1bb29593ea3001df3a36153dbc6
Suryaa <suryaa.prakash0369@gmail.com>
2025-09-22 23:02:53 +0530

    added three new ml model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print('here we add the ml model codegit')
+
+def model():
+    print("this is the model function") 
+
+def new_function():
+    print("this is a new function added in the ml model file")  
+
+def another_function():
+    print("this is another function added in the ml model file")        
